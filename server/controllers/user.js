1e3b6d09fca482f8621e1447402ff28e6811692a
Phco23 <142793213+Phco23@users.noreply.github.com>
2024-07-05 14:28:52 +0700

    v9

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,9 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
+const {generateAccessToken, generateRefreshToken} = require('../middlewares/jwt') 
+const jwt =  require('jsonwebtoken')
+const sendEmail = require('../utils/send_mail')
+const crypto = require('crypto')
 
 const register = asyncHandler(async(req, res) => {
     const {email, password, firstname, lastname} = req.body
@@ -8,13 +12,182 @@ const register = asyncHandler(async(req, res) => {
         success: false,
         mes: 'Missing inputs'
     })
-    const response = await User.create(req.body)
+
+    const user = await User.findOne({email})
+    if(user) throw new Error('User has already registed')
+    else{
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ? 'Register successfully. Go to login' : 'Something went wrong'
+        })
+    }
+})
+
+const login = asyncHandler(async(req, res) => {
+    const {email, password, firstname, lastname} = req.body
+    if(!email || !password)
+    return res.status(400).json({
+        success: false,
+        mes: 'Missing inputs'
+    })
+
+    const response = await User.findOne({email})    
+    if (response && await response.isCorrectPassword(password)){
+        // exclude password & role from response
+        const {password, role, refreshToken,...userData} = response.toObject()
+        // create access token
+        const accessToken = generateAccessToken(response._id, role)
+        // create refresh token
+        const newRefreshToken = generateRefreshToken(response._id)
+        // save refresh token to database
+        await User.findByIdAndUpdate(response._id, {refreshToken: newRefreshToken}, {new: true})
+        //save refresh token to cookie
+        res.cookie('refreshToken', newRefreshToken, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000})
+        return res.status(200).json({
+            success: true,
+            accessToken,
+            userData
+        })
+    }else{
+        throw new Error('Invaild credentials!')
+    }
+})
+
+const getCurrent = asyncHandler(async(req, res) => {
+    const {_id} = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: user ? true : false,
+        rs: user ? user : 'User not found'
+    })
+})
+
+const refreshAccessToken = asyncHandler(async(req, res) => {
+    // get token from cookies
+    const cookie = req.cookies
+    // check if has token or not
+    if(!cookie && !cookie.refreshToken) throw new Error('No refresh token in cookies')
+    // check token if vaild
+    const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET)
+    const response = await User.findOne({_id: rs._id, refreshToken: cookie.refreshToken})
+        return res.status(200).json({
+            success: response ? true : false,
+            newAccessToken: response ? generateAccessToken( response._id, response.role) : 'Refresh token invaild'
+        })
+})
+
+const logout = asyncHandler(async(req, res) => {
+    const cookie = req.cookies
+    if(!cookie || !cookie.refreshToken) throw new Error('No refresh token in cookies')
+    // delete refresh token from db
+    await User.findOneAndUpdate({refreshToken: cookie.refreshToken}, {refreshToken: ''}, {new: true})
+    // delete refresh token from browser's cookies
+    res.clearCookie('refreshToken', { 
+        httpOnly: true,
+        secure: true
+    })
+    return res.status(200).json({
+        success: true,
+        mes: 'Logout is done'
+    })
+})
+
+// client send email
+// server check email if vaild or not => send mail to email + link (change password)
+// client check email => click link
+// client send api to get token
+// check token is match to token from link server send
+// change password
+
+const forgotPassword = asyncHandler(async(req, res) => {
+    const {email} = req.query
+    if(!email) throw new Error('Missing email')
+    const user = await User.findOne({ email })
+    if(!user) throw new Error('User not found')
+    const resetToken = user.createPasswordChangedToken()
+    await user.save()
+
+    const html = `Please click the link here to change password.This link will expire 15 minutes from the time this email is created.
+    <a href=${process.env.URL_SERVER}/api/user/reset-password/${resetToken}>Click here</a>`
+
+    const data = {
+        email,
+        html
+    }
+    const rs = await sendEmail(data)
+    console.log(rs)
+    return res.status(200).json({
+        success: true,
+        rs
+    })
+})
+
+const resetPassword = asyncHandler(async (req, res) => {
+    const {password, token} = req.body
+    if(!password || !token) throw new Error('Missing inputs')
+    const passwordResetToken = crypto.createHash('sha256').update(token).digest('hex')
+    const user = await User.findOne({passwordResetToken, passwordResetExpires: {$gt: Date.now()}})
+    if(!user) throw new Error('Invaild reset token')
+    user.password = password
+    user.passwordResetToken = undefined
+    user.passwordChangedAt = Date.now()
+    user.passwordResetExpires = undefined
+    await user.save()
+    return res.status(200).json({
+        success: user ? true : false,
+        mes: user ? 'Updated password' : 'Something went wrong'
+    })
+})
+
+const getUsers = asyncHandler(async(req, res) => {
+    const response = await User.find().select('-refreshToken -password -role')
     return res.status(200).json({
         success: response ? true : false,
-        response
+        users: response
+    })
+})
+
+const deleteUser = asyncHandler(async(req, res) => {
+    const {_id} = req.query
+    if(!_id) throw new Error('Missing inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedUser: response ? `User with email ${response.email} deleted` : `No user deleted`
+    })
+})
+
+const updateUser = asyncHandler(async(req, res) => {
+    const {_id} = req.user
+    if(!_id || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body,  {new: true}).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : `Some thing went wrong`
+    })
+})
+
+const updateUserByAdmin = asyncHandler(async(req, res) => {
+    const {uid} = req.params
+    if(!uid || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(uid, req.body,  {new: true}).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : `Some thing went wrong`
     })
 })
 
 module.exports = {
-    register
+    register,
+    login,
+    getCurrent,
+    refreshAccessToken,
+    logout,
+    forgotPassword,
+    resetPassword,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,17 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verify_token')
 
 router.post('/register', ctrls.register)
+router.post('/login', ctrls.login)
+router.get('/current',verifyAccessToken, ctrls.getCurrent)
+router.post('/refresh-token', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
+router.get('/forgot-password', ctrls.forgotPassword)
+router.put('/reset-password', ctrls.resetPassword)
+router.get('/', verifyAccessToken, isAdmin, ctrls.getUsers)
+router.delete('/', verifyAccessToken, isAdmin, ctrls.deleteUser)
+router.put('/current', verifyAccessToken, ctrls.updateUser)
+router.put('/:uid', verifyAccessToken,isAdmin, ctrls.updateUserByAdmin)
 
 module.exports = router
